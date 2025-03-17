b9ed6265f760e22c1154c968dcd41f86b703eec5
mattgromines <mattgromines@gmail.com>
2025-03-16 22:28:55 -0600

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   src/stoplight_fsm.vhd

--- src/stoplight_fsm.vhd
+++ src/stoplight_fsm.vhd
@@ -77,10 +77,13 @@ architecture stoplight_fsm_arch of stoplight_fsm is
 begin
 	-- CONCURRENT STATEMENTS ----------------------------
 	-- Next state logic
-	
+	f_Q_next(0) <= not(f_Q(1)) and i_C;
+	f_Q_next(1) <= not(f_Q(1)) and f_Q(0) and not(i_C);
 	
 	-- Output logic
-	
+	o_G <= not(f_Q(1)) and f_Q(0);
+	o_Y <= f_Q(1) and not(f_Q(0));
+	o_R <= (not(f_Q(1)) and not(f_Q(0))) or (f_Q(1) and f_Q(0));
 	-------------------------------------------------------	
 	
 	-- PROCESSES ----------------------------------------	
